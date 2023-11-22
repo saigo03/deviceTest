cc3ead0e590343a8864dd3d91e65bac287fb5872
saigo03 <149645624+saigo03@users.noreply.github.com>
2023-11-22 00:57:19 +0000

    ログイン機能、マイページ実装まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "hello#index"
+
+  # Memosに関連するルートを追加
+  resources :memos
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_21_020033) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_21_051615) do
+  create_table "memos", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_memos_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_21_020033) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "memos", "users"
 end
