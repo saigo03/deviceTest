e253204bbd5e6fe6a123a614e27153e88ac70127
keitaro <147474084+Nakaharakeitaro@users.noreply.github.com>
2023-12-05 04:06:01 +0000

    お問合せ組み合わせ

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,11 @@ Rails.application.routes.draw do
   get 'hello_index', to: 'hello#index', as: 'hello_index'
   get 'mypage', to: 'static_pages#'
 
+  #お問合せ用ルート
+  resources :posts, only: [:new, :create, :index, :show] do
+    member do
+      get 'adminshow'
+    end
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_30_014011) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_30_045328) do
   create_table "memos", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title"
@@ -23,6 +23,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_014011) do
   create_table "memos_tags", id: false, force: :cascade do |t|
     t.integer "memo_id", null: false
     t.integer "tag_id", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tags", force: :cascade do |t|
